29231bbff73cae80a073ed4aaac4caf070c8a363
hlion17 <yourdad20626@gmail.com>
2022-06-22 01:32:13 +0900

    JPA, Hibernate, Spring Data Jpa RDBS를 객체지향적으로 다루는법 JPA 더티체킹 JPA Auditing

---
+++ src/main/java/me/hlion/book/service/posts/PostsService.java
@@ -0,0 +1,35 @@
+package me.hlion.book.service.posts;
+
+import lombok.RequiredArgsConstructor;
+import me.hlion.book.domain.posts.Posts;
+import me.hlion.book.domain.posts.PostsRepository;
+import me.hlion.book.dto.PostsResponseDto;
+import me.hlion.book.dto.PostsSaveRequestDto;
+import me.hlion.book.dto.PostsUpdateRequestDto;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+@RequiredArgsConstructor
+@Service
+public class PostsService {
+    private final PostsRepository postsRepository;
+
+    @Transactional
+    public Long save(PostsSaveRequestDto requestDto) {
+        return postsRepository.save(requestDto.toEntity()).getId();
+    }
+
+    @Transactional
+    public Long update(Long id, PostsUpdateRequestDto requestDto) {
+        Posts posts = postsRepository.findById(id)
+                .orElseThrow(() -> new IllegalArgumentException("해당 게시글이 없습니다. id=" + id));
+        posts.update(requestDto.getTitle(), requestDto.getContent());
+        return id;
+    }
+
+    public PostsResponseDto findById(Long id) {
+        Posts entity = postsRepository.findById(id)
+                .orElseThrow(() -> new IllegalArgumentException("해당 게시글이 없습니다. id=" + id));
+        return new PostsResponseDto(entity);
+    }
+}
---
+++ src/main/java/me/hlion/book/web/PostsApiController.java
@@ -0,0 +1,32 @@
+package me.hlion.book.web;
+
+import lombok.RequiredArgsConstructor;
+import me.hlion.book.domain.posts.PostsRepository;
+import me.hlion.book.dto.PostsResponseDto;
+import me.hlion.book.dto.PostsSaveRequestDto;
+import me.hlion.book.dto.PostsUpdateRequestDto;
+import me.hlion.book.service.posts.PostsService;
+import org.springframework.web.bind.annotation.*;
+
+@RequiredArgsConstructor
+@RestController
+public class PostsApiController {
+
+    private final PostsService postsService;
+
+    @PostMapping("/api/v1/posts")
+    public Long save(@RequestBody PostsSaveRequestDto requestDto) {
+        return postsService.save(requestDto);
+    }
+
+    @PutMapping("/api/v1/posts/{id}")
+    public Long update(@PathVariable Long id, @RequestBody PostsUpdateRequestDto requestDto) {
+        return postsService.update(id, requestDto);
+    }
+
+    @GetMapping("/api/v1/posts/{id}")
+    public PostsResponseDto findById(@PathVariable Long id) {
+        return postsService.findById(id);
+    }
+
+}
